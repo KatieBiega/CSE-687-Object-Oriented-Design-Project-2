292a3136a4faa039d01f11dafe1f21bd08dea0e2
17djoseph <147456465+17djoseph@users.noreply.github.com>
2023-11-10 20:20:42 -0500

    Update File Management.h

--- File Management.h
+++ File Management.h
@@ -12,11 +12,23 @@ public:
     void WriteToTempFile(const std::string& fileName, const std::string& data);
     void WriteToOutputFile(const std::string& fileName, const std::string& data);
 
+    template<typename T>
+    void WriteToTempOrOutputFile(const std::string& fileName, const std::string& data);
+
 private:
     std::string inputDirectory;
     std::string outputDirectory;
     std::string tempDirectory;
 };
+
+template<typename T>
+void FileManagement::WriteToTempOrOutputFile(const std::string& fileName, const std::string& data) {
+    T fileStream((tempDirectory + fileName).c_str());
+    if (fileStream.is_open()) {
+        fileStream << data;
+        fileStream.close();
+    }
+}
 
 #endif
 
