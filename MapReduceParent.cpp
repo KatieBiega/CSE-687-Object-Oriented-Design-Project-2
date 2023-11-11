867561179397af5744fbdcfa2ed2513c07003571
frmuvto <124471166+frmuvto@users.noreply.github.com>
2023-11-10 19:12:13 -0500

    Add files via upload

---
+++ MapReduceParent.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+#include "MapReduceParent.h"
+
+string MapReduceParent::vector_export() {
+
+}
---
+++ MapReduceParent.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+class MapReduceParent {
+public:
+
+	virtual string vector_export();
+
+private:
+
+};
